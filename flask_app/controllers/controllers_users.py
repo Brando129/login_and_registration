7e34650c03d0d3e1edf5f0610b8c569c4bd0d2e3
Brandon Colangelo <brandoncancode@gmail.com>
2023-06-29 11:55:03 -0400

    building route for registering user

--- flask_app/controllers/controllers_users.py
+++ flask_app/controllers/controllers_users.py
@@ -1,8 +1,20 @@
 from flask_app import app
 from flask import render_template, redirect, request
+from flask_app.models import models_user
 
 
+# Get Routes
 # Route for rendering the "Dashboard Page"
 @app.route('/')
 def index():
     return render_template('dashboard.html')
+
+# Post Routes
+# Route for registering a user
+@app.route('/register', methods=['POST'])
+def register():
+    if not models_user.User.validate_user(request.form):
+        # We redirect to the template with the form.
+        return redirect('/')
+
+    return redirect('/dashboard')
